2b8e5878aa19e5ed93fa0c6d39fdff0c5ac833cb
Carlos <carlos.santos74@outlook.com>
2023-08-21 21:20:07 -0300

    feat: add initial structure

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <Html>
+      <Head>
+        <Title>The cocktail</Title>
+      </Head>
+      <Body>
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+      </Body>
+    </Html>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    Home page here!
+  </div>
+</template>
