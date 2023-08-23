684cf4aed613692a7026508b1b5dd7535e516aa3
Carlos <carlos.santos74@outlook.com>
2023-08-22 22:31:43 -0300

    feat: add 404 page

---
+++ pages/[...slug].vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <h1 class="q-mb-md">
+      404
+    </h1> 
+    <p>
+      Page not found
+    </p>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  section { 
+    height: 100vh;
+    text-align: center;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background-color: var(--violet-3);
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    Home page here!
-  </div>
+  <div></div>
 </template>
