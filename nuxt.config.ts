2959ca1f4fd454b3e0fb4c8a113c2939d2aefb90
Carlos <carlos.santos74@outlook.com>
2023-08-22 22:29:44 -0300

    chore(config): disable dev-tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { 
-    enabled: true
+    enabled: false
    },
    css: [
     '@/assets/styles/global.scss'
-   ]
+   ],
 })
