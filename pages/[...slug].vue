88bc688d4778a094d56017a6588b4c6b8c305bb1
Carlos <carlos.santos74@outlook.com>
2023-08-27 14:37:20 -0300

    feat: add back to home button on 404 page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,26 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const router = useRouter();
+</script>
 
 <template>
   <section>
+    <Icon size="32" name="mdi:glass-cocktail-off" />
     <h1 class="q-mb-md">
       404
     </h1> 
     <p>
       Page not found
     </p>
+
+    <button class="cs-button__secondary" @click="router.push('/')">
+      <Icon name="material-symbols:arrow-back" /> 
+      Go back to home
+    </button>
   </section>
 </template>
 
 <style scoped lang="scss">
   section { 
-    height: 100vh;
+    height: 90vh;
     text-align: center;
 
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    gap: 8px;
 
-    background-color: var(--violet-3);
+    button { 
+      margin-top: 24px;
+    }
   }
 </style>
